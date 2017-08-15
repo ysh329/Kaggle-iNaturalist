15e956611ef0a77e2014758c56f307b99ca510fc
ysh329@sina.com <ysh329@sina.com>
2017-08-15 01:18:14 +0000

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-export MXNET_CPU_WORKER_NTHREADS=24
+export MXNET_CPU_WORKER_NTHREADS=48
 export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
-python fine-tune.py --pretrained-model model/resnet-152 \
-    --load-epoch 0 --gpus 0,1,2,3 \
+python fine-tune.py --pretrained-model model/iNat-resnet-152 \
+    --load-epoch 3 --gpus 0,1,2 \
 	--model-prefix model/iNat-resnet-152 \
-	--data-nthreads 48 \
-    --batch-size 48 --num-classes 5089 --num-examples 579184
+	--data-nthreads 24 \
+    --batch-size 20 --num-classes 5089 --num-examples 579184
